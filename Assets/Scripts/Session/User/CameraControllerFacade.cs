c18a126c787a282cc5f93912745517e34362930d
ErOPKO3AOB <92738166+ErOPKO3AOB@users.noreply.github.com>
2025-12-22 15:43:47 +0300

    Concepts of Session UI

---
+++ Assets/Scripts/Session/User/CameraControllerFacade.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace Session.User
+{
+    public class CameraControllerFacade : MonoBehaviour
+    {
+        [SerializeField] private Camera _camera;
+        [SerializeField] private float _sensitivity;
+
+        private void Update()
+        {
+            
+        }
+    }
+}
